ef30e0d86ff2223158f6ee9b985769bcc89970ab
ahmedcpp6038-max <ahmed.cpp6038@gmail.com>
2026-06-19 11:32:06 +0100

    added comments

--- boom.js
+++ boom.js
@@ -1,8 +1,11 @@
+// giving a value to the element
+
 const title1= document.getElementById("title1");
 const mypara1= document.getElementById("para1");
 const title4= document.getElementById("title4");
 const link= document.getElementById("link");
 
+// changing the content of the elements
 mypara1.innerHTML = "this is a para from JS";
 title1.innerHTML = "this is a title";
 title4.innerHTML="this is a smaller title from js";
