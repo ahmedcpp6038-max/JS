0a77f31eb2a41d3c52ccb2b1c72ade0795a465ca
ahmedcpp6038-max <ahmed.cpp6038@gmail.com>
2026-06-19 11:02:58 +0100

    my first js

---
+++ boom.js
@@ -0,0 +1,11 @@
+const title1= document.getElementById("title1");
+const mypara1= document.getElementById("para1");
+const title4= document.getElementById("title4");
+const link= document.getElementById("link");
+
+mypara1.innerHTML = "this is a para from JS";
+title1.innerHTML = "this is a title";
+title4.innerHTML="this is a smaller title from js";
+link.innerHTML="press on this link";
+
+
